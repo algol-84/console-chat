e407a3110337333978d986e6fc4837a43bf687ab
algol <algol.py@gmail.com>
2024-10-14 10:59:15 +0300

    fix ci deploy

--- auth_pg/migration.sh
+++ auth_pg/migration.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source .env
-@echo ">  Run migration script...."
-export MIGRATION_DSN="host=auth-db-container port=5432 dbname=$PG_DATABASE_NAME user=$PG_USER password=$PG_PASSWORD sslmode=disable"
+
+export MIGRATION_DSN="host=${DB_HOST:-pg_auth} port=5432 dbname=$PG_DATABASE_NAME user=$PG_USER password=$PG_PASSWORD sslmode=disable"
 
 sleep 2 && goose -dir "${MIGRATION_DIR}" postgres "${MIGRATION_DSN}" up -v
