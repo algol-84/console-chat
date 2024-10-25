62a062d5d73bc70a3bf94d7dae77ab73fb1f6708
algol <algol.py@gmail.com>
2024-10-25 13:53:16 +0300

    fixed linter bug

--- internal/api/auth/get.go
+++ internal/api/auth/get.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/algol-84/auth/internal/converter"
 	desc "github.com/algol-84/auth/pkg/user_v1"
-)	
+)
 
 // Get обрабатывает GRPC запросы на получение данных пользователя
 func (i *Implementation) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
